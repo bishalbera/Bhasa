0741cdc99fe15fe533ae08fd374a6a7e5ee1cfe7
Bishal <bishalbera2@gmail.com>
2023-08-05 15:52:05 +0530

    profile and badges screen added

--- lib/presentation/screens/Home/home_screen.dart
+++ lib/presentation/screens/Home/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: MyCustomBottomNavigationBar(),
+        bottomNavigationBar: const MyCustomBottomNavigationBar(),
         appBar: AppBar(
           title: const Text('Home'),
           backgroundColor: Colors.blueAccent,
